f1435b72e76e074d08547cd778ac43d7d51ae0b1
BadWolf <phil@pichet.eu>
2017-11-30 11:09:52 +0100

    Fixing getPerfect condition

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 var form = document.getElementById('select-quantity');
-var ulElement = document.getElementById('perfect-list')
-console.log(ulElement)
+var ulElement = document.getElementById('perfect-list');
+
 
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
@@ -19,6 +19,7 @@ form.addEventListener("submit", function (event) {
 	} else {
 		alert("Veuillez rentrer un chiffre compris entre 1 et 8");
 	}
+	document.getElementById("quantity").value = ""
 	return false;
 })
 
@@ -26,36 +27,34 @@ form.addEventListener("submit", function (event) {
 
 function isPrime(x) {
 	racine = Math.sqrt(x);
-        var b = 1;
-                    do {
-                        b++;
-                    } while (b <= racine && x%b != 0);
-                    
-                if (x == 2 && x%2 != 0 && !Number.isInteger(racine)) 
-                    {
-                        return true;  
-                    } else if(b > racine)
-		    {
-			return true;                  
-                    }
-        else return false;
+	var b = 1;
+	do {
+		b++;
+	} while (b <= racine && x%b != 0);
+
+	if ((x == 2 && x%2 != 0 && !Number.isInteger(racine)) || b > racine) {
+		return true;  
+	} 
+	return false;
 }
 
 
 function getPerfects(number) {
-	// Create array for storage
-	var perfectNumbers = [];
+
+// Create array for storage
+var perfectNumbers = [];
 	var i = 2
 	// We make a boucle for the p value
 	while(true) {
 
 		first = Math.pow(2, i- 1); 
 		second = Math.pow(2, i) - 1;
-		
 		// if first and second are prime we have this form
 		// perfect = 2exp(i-1)exp(2exp(i))
-		if(isPrime(first) && isPrime(second)){
-			perfectNumbers.push( first * second)
+		if(isPrime(i) && isPrime(second)){
+
+			// We store the number with key is the value of i
+			perfectNumbers.push(first * second)
 		}
 		// We have the number of perfect number
 		if(perfectNumbers.length == number) {
@@ -76,9 +75,7 @@ function displayPerfectList(perfectList) {
 		// We create a li element for content a perfect number and we display it in ul list
 		var liElement = document.createElement("li");
 		liElement.innerHTML = perfectNumber;
-
-		
-
 		ulElement.append(liElement);
 	} )
 }
+
